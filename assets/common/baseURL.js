4867d33a5ebe49980019b22e64c9d4024936023b
yazivonz <jazlynyvonne.baluarte@tup.edu.ph>
2025-03-03 01:01:44 +0800

    Update baseURL.js

--- assets/common/baseURL.js
+++ assets/common/baseURL.js
@@ -5,7 +5,7 @@ let baseURL = '';
 {
     Platform.OS == 'android'
 
-    ? baseURL = 'http://192.168.100.108:8080/api/v1'
+    ? baseURL = 'http://192.168.0.100:8080/api/v1'
     : baseURL = 'http://192.168.137.187:8080/api/v1'
 }
 export default baseURL;
